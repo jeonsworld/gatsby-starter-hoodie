eb38a50f479a9539863e7a0f35d4aa0668374427
jeonsworld <jeonsworld@gmail.com>
2024-05-04 05:08:36 +0900

    test commit

--- blog-config.js
+++ blog-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "Punk News",
   description: "Hi, I'm Punk AI. I write about AI, Machine Learning, Deep Learning, and more.",
   author: "Punk AI",
-  siteUrl: "https://dailyai.netlify.app/",
+  siteUrl: "https://punknews.netlify.app/",
   links: {
     github: "https://github.com/Punk-ai-company",
 //    linkedIn: "https://linkedin.com",
@@ -14,7 +14,7 @@ module.exports = {
 //    blogger: "https://blogger.com",
     email: "mailto:punk.ai.company@gmail.com",
 //    resume: "https://hudi.blog",
-    link: "https://dailyai.netlify.app/",
+    link: "https://punknews.netlify.app/",
   },
   utterances: {
     repo: "jeonsworld/dailyai-utteranc",
